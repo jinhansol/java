e6b418b0617da1228a4e83f58e9692144ad5e517
Han <jinhan9657@naver.com>
2025-05-26 11:21:57 +0900

    250526 Java 실습(오전 실습)

--- EqualsEx.java
+++ EqualsEx.java
@@ -1,14 +1,14 @@
-class Point {
-    private int x, y;
-    public Point(int x, int y) {
-        this.x = x; this.y = y;
-    }
-    public boolean equals(Object obj) {
-        Point p = (Point)obj;
-        if(x == p.x && y == p.y) return true;
-        else return false;
-    }
-}
+//class Point {
+//    private int x, y;
+//    public Point(int x, int y) {
+//        this.x = x; this.y = y;
+//    }
+//    public boolean equals(Object obj) {
+//        Point p = (Point)obj;
+//        if(x == p.x && y == p.y) return true;
+//        else return false;
+//    }
+//}
 
 class Rect {
     private int width;
